93eceeb938054fcdb996744c08483fe3e7298bb4
sgavil <ssjsrgx@gmail.com>
2025-03-02 00:36:10 +0100

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cooking-recipes/',  // Should match your repository name
+  base: '/cooking-api-web/', // for GitHub Pages
 }) 
