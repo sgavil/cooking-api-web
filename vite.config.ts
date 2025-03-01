0c5ccc756e99422d19f499c52f43b9e0645a2493
sgavil <ssjsrgx@gmail.com>
2025-03-02 00:45:16 +0100

    actions not working..

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cooking-api-web/', // for GitHub Pages
-}) 
+  base: '/cooking-api-web/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'esbuild',
+    reportCompressedSize: false,
+  },
+})
